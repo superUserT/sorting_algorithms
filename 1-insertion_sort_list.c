88022fbd2a12daa5dc611aff36933fda37564750
superUserT <rantshothabisomail@gmail.com>
2023-09-26 09:41:56 +0200

    insertion sort, betty checks

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order  * using the Insertion sort algorithm.
+ * insertion_sort_list - Sorts list using the Insertion sort.
  * @list: A pointer to a pointer to the head of the linked list
  */
 void insertion_sort_list(listint_t **list)
